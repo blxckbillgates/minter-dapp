b372dd7ee555feb4dd16b70379b21f281c284201
Usman Tijani <usmantijani17@gmail.com>
2022-05-07 15:16:05 -0400

    Constant Js update

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x1562FCAad350593B2b5fD38A53b2e061Eaabdb3e"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Greenwood District NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
