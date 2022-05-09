61183bf625d7c7f1f31d9e525d9932a6a70f5ab7
Usman Tijani <usmantijani17@gmail.com>
2022-05-08 21:56:23 -0400

    contract update

--- backend/src/config.js
+++ backend/src/config.js
@@ -87,7 +87,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = []; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x48631c6281c2BC4bD4b2d52Ab4b0DE323a22B8ee"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
